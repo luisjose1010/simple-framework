d3411df04d2493e9cb47ea775d18f670ae83a247
Aloysius1010 <luis06jose@gmail.com>
2021-07-07 17:54:07 -0400

    Agregado un método estático en la clase Controller con la finalidad de redireccionar desde un controlador, o incluso desde otras partes del código

--- framework/Controller.php
+++ framework/Controller.php
@@ -5,4 +5,9 @@ namespace Framework;
 abstract class Controller
 {
     public abstract function index(array $parameters);
+
+    public static function redirect($route){
+        echo "<script>window.location.href = '$route'</script>";
+        exit();
+    }
 }
